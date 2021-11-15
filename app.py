4c3c763485cad6313b2dad559df359f45dbf9194
Stephane Rongier <stephane.rongier@hotmail.com>
2021-11-15 18:32:10 +0100

    ok

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 from bbquote.lib import get_quote
 
-#author, quote = get_quote()  # assuming the function returns an author and a quote
+author, quote = get_quote()  # assuming the function returns an author and a quote
 
-#f"{quote}, {author}"
+f"{quote}, {author}"
 
 
